0627d74e94249397e2a5ed6ef847fbcc796183f4
Alphonzo Escolar <phonz.dev@gmail.com>
2025-05-24 10:47:19 +0800

    complete exercise 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,6 +1,15 @@
 import { useState } from "react";
 
 const Statistics = ({ good, neutral, bad, total, average, positive }) => {
+	if (total === 0) {
+		return (
+			<>
+				<h2>statistics</h2>
+				<p>No feedback given</p>
+			</>
+		);
+	}
+
 	return (
 		<>
 			<h2>statistics</h2>
