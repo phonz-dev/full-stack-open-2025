f89a14bf28b9a9a42128eb35f82d662f6cfbfef7
Alphonzo Escolar <phonz.dev@gmail.com>
2025-05-24 16:55:55 +0800

    complete exercise 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -8,6 +8,12 @@ const App = () => {
 
   const addPerson = event => {
     event.preventDefault();
+
+    if (persons.some(({ name }) => name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+
     const newPerson = {
       name: newName
     }
