ab3abebbcfd9fcf156a34d96ec16113080996c0f
Alphonzo Escolar <phonz.dev@gmail.com>
2025-05-24 11:43:45 +0800

    complete exercise 1.14

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -10,39 +10,57 @@ const App = () => {
 		"Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
 		"Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
 		"The only way to go fast, is to go well.",
-  ];
+	];
 
-  const initialVotesState = anecdotes.reduce((obj, _, i) => {
-    if (obj[i] === undefined) {
-      obj[i] = 0
-    } else {
-      obj[i] += 1;
-    }
-    return obj;
-  }, {})
+	const initialVotesState = anecdotes.reduce((obj, _, i) => {
+		if (obj[i] === undefined) {
+			obj[i] = 0;
+		} else {
+			obj[i] += 1;
+		}
+		return obj;
+	}, {});
 
-
-  const [selected, setSelected] = useState(0);
-  const [votes, setVotes] = useState(initialVotesState);
-
-  const voteAnecdote = () => {
-    const votesCopy = { ...votes }
-    votesCopy[selected] += 1
-    setVotes(votesCopy)
-  }
+	const [selected, setSelected] = useState(0);
+	const [votes, setVotes] = useState(initialVotesState);
 
 	const selectRandomAnecdote = () => {
-    let randomIndex = selected;
-    while (randomIndex === selected) randomIndex = Math.floor(Math.random() * Math.floor(anecdotes.length))
-    setSelected(randomIndex);
+		let randomIndex = selected;
+		while (randomIndex === selected)
+			randomIndex = Math.floor(Math.random() * Math.floor(anecdotes.length));
+		setSelected(randomIndex);
+	};
+
+	const getIndexWithMostVotes = () => {
+		let mostVotes = 0;
+		let mostVotesIndex = 0;
+		for (const index in votes) {
+			const value = votes[index];
+			if (value > mostVotes) {
+				mostVotes = value;
+				mostVotesIndex = index;
+			}
+		}
+		return mostVotesIndex;
+	};
+
+	const voteAnecdote = () => {
+		const votesCopy = { ...votes };
+		votesCopy[selected] += 1;
+		setVotes(votesCopy);
 	};
 
 	return (
 		<div>
-      <p>{anecdotes[selected]}</p>
-      <p>has { votes[selected] } votes</p>
-      <button onClick={voteAnecdote}>vote</button>
+			<h3>Anecdote of the day</h3>
+			<p>{anecdotes[selected]}</p>
+			<p>has {votes[selected]} votes</p>
+			<button onClick={voteAnecdote}>vote</button>
 			<button onClick={selectRandomAnecdote}>next anecdote</button>
+
+			<h3>Anecdote with most votes</h3>
+			<p>{anecdotes[getIndexWithMostVotes()]}</p>
+			<p>has {votes[getIndexWithMostVotes()]} votes</p>
 		</div>
 	);
 };
