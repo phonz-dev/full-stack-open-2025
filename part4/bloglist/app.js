0a6a238172d1b07fe72a09a1e1d09bd523a50c42
Alphonzo Escolar <phonz.dev@gmail.com>
2025-05-28 21:57:22 +0800

    complete exercise 4.1 and 4.2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+const app = express()
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => logger.info('connected to MongoDB'))
+  .catch(error => logger.error(`error connecting to MongoDB ${error.message}`))
+
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,20 +1,7 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const blogsRouter = require('./controllers/blogs')
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const app = express()
-
-const mongoUrl = 'mongodb+srv://fullstack:learningmongo@cluster0.mrk1y3h.mongodb.net/blogListApp?retryWrites=true&w=majority&appName=Cluster0'
-mongoose
-  .connect(mongoUrl)
-  .then(() => console.log('connected to MongoDB'))
-  .catch(error => console.error(`error connecting to MongoDB ${error.message}`))
-
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
