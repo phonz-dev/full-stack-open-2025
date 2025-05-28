0f8eb8713f1a560283278b7b2df6d018e306cfb9
Alphonzo Escolar <phonz.dev@gmail.com>
2025-05-28 13:51:21 +0800

    complete exercise 3.20

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -16,20 +16,31 @@ mongoose
 	});
 
 const personSchema = mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
-    },
-	number: String,
+	name: {
+		type: String,
+		minLength: 3,
+		required: true,
+	},
+	number: {
+		type: String,
+		minLength: 8,
+		validate: {
+			validator: function (v) {
+				return /^\d{2,3}-.{4,}$/.test(v);
+			},
+			message: (props) =>
+				`${props.value} is not a valid phone number! It should start with 2 or 3 digits, followed by a dash, and at least 4 more characters.`,
+        },
+        required: [true, 'User phone number required']
+	},
 });
 
-personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v;
-    }
-})
+personSchema.set("toJSON", {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString();
+		delete returnedObject._id;
+		delete returnedObject.__v;
+	},
+});
 
 module.exports = mongoose.model("Person", personSchema);
