caa0c3e35551a41b2e4528ef8b6a07b92ac6afee
Alphonzo Escolar <phonz.dev@gmail.com>
2025-05-29 20:56:47 +0800

    complete exercise 4.13

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -70,7 +70,7 @@ test('if likes property is missing, likes\' value default to zero', async () => 
   assert.strictEqual(response.body.likes, 0)
 })
 
-test.only('responds with 400 status code if the title or url properties are missing', async () => {
+test('responds with 400 status code if the title or url properties are missing', async () => {
   let blog = {
     author: "Grant Cardone",
     url: "https://grantcardone.com/",
@@ -94,6 +94,21 @@ test.only('responds with 400 status code if the title or url properties are miss
   const response = await api.get('/api/blogs')
   assert.strictEqual(response.body.length, helper.initialBlogs.length)
 })
+
+test.only('deletes a specific blog', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToDelete = blogsAtStart[0]
+
+  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+
+  const blogsAtEnd = await helper.blogsInDb()
+
+  const titles = blogsAtEnd.map(({ title }) => title)
+  assert(!titles.includes(blogToDelete.title))
+
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+})
+
 
 after(() => {
   mongoose.connection.close()
