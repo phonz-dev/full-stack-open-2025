5cd9fcd1baf00288af7e3421d2155f0b1a8d4d51
Alphonzo Escolar <phonz.dev@gmail.com>
2025-05-29 19:50:33 +0800

    complete exercises 4.8 - 4.10

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,57 @@
+const { test, beforeEach, after } = require('node:test')
+const assert = require('node:assert')
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const helper = require('./test_helper')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+test('all blogs are returned', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length)
+})
+
+test('all blogs has an \'id\' parameter', async () => {
+  const response = await api.get('/api/blogs')
+
+  assert(response.body.every((blog) => blog.id !== undefined))
+  assert(response.body.every((blog) => blog._id === undefined))
+})
+
+test('successfully creates a valid blog', async () => {
+  const blog = {
+    title: "How to learn fullstack development",
+    author: "Alphonzo Escolar",
+    url: "https://computernerds.com/",
+    likes: 101,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(blog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  let response = await api.get('/api/blogs')
+  assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)
+
+  const titles = response.body.map(({ title }) => title)
+  assert(titles.includes('How to learn fullstack development'))
+})
+
+after(() => {
+  mongoose.connection.close()
+})
--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 
-const PORT = 3003
-const MONGODB_URI = 'mongodb+srv://fullstack:learningmongo@cluster0.mrk1y3h.mongodb.net/blogListApp?retryWrites=true&w=majority&appName=Cluster0'
+const PORT = process.env.PORT
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
 
 module.exports = { PORT, MONGODB_URI }
