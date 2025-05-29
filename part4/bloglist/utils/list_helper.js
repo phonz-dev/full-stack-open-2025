e6a568fd243792d6a718aac4fbdd177c71365b9f
Alphonzo Escolar <phonz.dev@gmail.com>
2025-05-29 13:01:47 +0800

    complete exercise 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -5,7 +5,20 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) =>
 	blogs.reduce((total, { likes }) => total + likes, 0);
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+
+  return blogs.reduce((blog1, blog2) => {
+    if (blog2 === undefined || blog1.likes > blog2.likes) {
+      return blog1
+    }
+
+    return blog2
+  })
+};
+
 module.exports = {
 	dummy,
-	totalLikes,
+  totalLikes,
+  favoriteBlog
 };
