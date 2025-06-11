ab2fcecbcd0c25f204df7779b3b8a769cbe16361
Alphonzo Escolar <phonz.dev@gmail.com>
2025-06-11 14:21:07 +0800

    complete exercise 8.2

--- part8/books-and-authors/index.js
+++ part8/books-and-authors/index.js
@@ -98,18 +98,26 @@ let books = [
 */
 
 const typeDefs = `
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]
+  }
+
   type Query {
-    dummy: Int
     bookCount: Int
     authorCount: Int
+    allBooks: [Book!]
   }
 `
 
 const resolvers = {
   Query: {
-    dummy: () => 0,
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
